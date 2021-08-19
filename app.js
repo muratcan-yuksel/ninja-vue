f19d18c0d7e80a7f32f61cf4226958b914323162
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-19 17:24:03 +0300

    hide books button added

--- app.js
+++ app.js
@@ -5,22 +5,17 @@ const app = Vue.createApp({
   // template: "<h2>I'm the template </h2>",
   data() {
     return {
+      showBooks: true,
       title: "empire strikes back",
       theDude: "Darth Vader",
       year: 1433,
     };
   },
   methods: {
-    changeTitle(titre) {
-      // console.log("you clciked");
-      //"this" represents the entire app component
-      // this.title = "brudas of peace";
-      this.title = titre;
+    toggle() {
+      this.showBooks = false;
     },
   },
 });
 //mount that app to this html element with and id of "app"
 app.mount("#app");
-// woooow. I have a real html saying something different, and whenever I comment out the tamplate in const app,
-//this one overwrites the html I wrote in index.html
-//That means dynamic updates
