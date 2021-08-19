3eae35da39691497233cc7a5bcb3be8eaab54961
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-19 16:27:28 +0300

    ....

---
+++ app.js
@@ -0,0 +1,16 @@
+//create a vue app
+//you can use any name for the constant
+//translation: this says create a new app and then... (go below)
+const app = Vue.createApp({
+  // template: "<h2>I'm the template </h2>",
+  data() {
+    return {
+      title: "empire strikes back",
+    };
+  },
+});
+//mount that app to this html element with and id of "app"
+app.mount("#app");
+// woooow. I have a real html saying something different, and whenever I comment out the tamplate in const app,
+//this one overwrites the html I wrote in index.html
+//That means dynamic updates
