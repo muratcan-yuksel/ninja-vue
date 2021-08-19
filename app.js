237bace48dbf3d0eb62c96f7c16d2b1f5b241a3d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-19 17:37:14 +0300

    readded appjs

---
+++ app.js
@@ -0,0 +1,22 @@
+//create a vue app
+//you can use any name for the constant
+//translation: this says create a new app and then... (go below)
+const app = Vue.createApp({
+  // template: "<h2>I'm the template </h2>",
+  data() {
+    return {
+      showBooks: true,
+      title: "empire strikes back",
+      theDude: "Darth Vader",
+      year: 1433,
+    };
+  },
+  methods: {
+    toggle() {
+      //reverse toggle button :D
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
+//mount that app to this html element with and id of "app"
+app.mount("#app");
